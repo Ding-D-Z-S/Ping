e2d0012b478f4665f66062da40c069ffa656efac
Yang_YG <109493302+YHQ0214@users.noreply.github.com>
2023-06-03 11:22:39 +0800

    Delete clear_output.py

--- clear_output.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-#用于清空输出栏的内容
-def clear_output(text_output):
-    text_output.config(state=tk.NORMAL)
-    text_output.delete(1.0, tk.END)
-    text_output.config(state=tk.DISABLED)
